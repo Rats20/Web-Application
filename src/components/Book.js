e7ed06dede9d675460d33f468638e82b41462f6d
Rats20 <75200584+Rats20@users.noreply.github.com>
2020-12-03 09:31:45 +0530

    Update Book.js

--- src/components/Book.js
+++ src/components/Book.js
@@ -20,30 +20,39 @@ class Book extends React.Component{
             float : "left"
         };
 
-        var letterStyle = {
+        var letterStyle={
             fontFamily : "Arial, Times, Lucida Grande",
             fontSize : 20,
             margin : 20
         };
 
-        var buttonStyle = {
-            backgroundColor : "#F5E5D6",
-            border : "none",
-            width : 300,
-            height : 50,
-            fontFamily : "American Typewriter, Times New Roman, sans-serif",
-            fontSize : 24,
-            fontWeight : "normal",
-            textAlign : "center",
-            marginLeft : 10
-        };
+        var stockStyle;
+
+        if(this.props.stock === "In stock"){
+            stockStyle = {
+                fontFamily : "Arial, Times, Lucida Grande",
+                fontSize : 20,
+                color : "green",
+                margin : 20
+            };
+        }
+        else
+        {
+            stockStyle = {
+                fontFamily : "Arial, Times, Lucida Grande",
+                fontSize : 20,
+                color : "red",
+                margin : 20
+            };
+        }
 
         return(
             <div style={boxStyle}>
                 <img src={this.props.source} alt="Book Image" style={imgStyle}/>
                 <p style={letterStyle}>{this.props.tbName}</p>
                 <p style={letterStyle}>Book owner : {this.props.owner}</p>
-                <button style={buttonStyle}>More Information?</button>
+                <p style={letterStyle}>Contact Details : {this.props.phno}</p>
+                <p style={stockStyle}>Availability : {this.props.stock}</p>
             </div>
         );
     }
