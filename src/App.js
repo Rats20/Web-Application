46b11e3c0ed381c1b3e9f09362e854bc5a8c2d19
Rats20 <75200584+Rats20@users.noreply.github.com>
2020-12-03 09:36:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,15 +3,8 @@ import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import Header from './components/Header';
 import Login from './components/Login';
 import CategoryMain from './components/Categorymain';
+import SnHBooks from './components/SnHBooks';
 import CSEBooks from './components/CSEBooks';
-
-/*
-<div style={{float : "left", marginTop : 120, fontSize : 50}}>
-  <NavLink exact to="/login">Login</NavLink>
-</div>
-s*/
-
-//withRouter not working!
 
 class App extends React.Component{
   render(){
@@ -22,8 +15,14 @@ class App extends React.Component{
         <Switch>
           <Route exact path="/" component={Login}/>
           <Route path="/categorymain" component={CategoryMain}/>
-          <Route path="/snh" component={CSEBooks}/>
+          <Route path="/snh" component={SnHBooks}/>
           <Route path="/cse" component={CSEBooks}/>
+          <Route path="/ece" component={CSEBooks}/>
+          <Route path="/eee" component={CSEBooks}/>
+          <Route path="/me" component={CSEBooks}/>
+          <Route path="/civ" component={CSEBooks}/>
+          <Route path="/bt" component={CSEBooks}/>
+          <Route path="/others" component={CSEBooks}/>
         </Switch>
       </Router>
       </div>
