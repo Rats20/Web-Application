fe65e126032d609b543817abf92b1613e99b8740
Rats20 <75200584+Rats20@users.noreply.github.com>
2020-12-07 20:47:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-mport React from'react';
+import React from'react';
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import Header from './components/Header';
 import Login from './components/Login';
