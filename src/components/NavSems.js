6735a7e29edc8e0e58115907a541038a2379c8ad
Rats20 <75200584+Rats20@users.noreply.github.com>
2020-12-09 21:09:52 +0530

    Update NavSems.js

--- src/components/NavSems.js
+++ src/components/NavSems.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import MenuIcon from './img/MenuIcon.jpeg';
+import LeftArrow from './img/LeftArrow.jpeg';
+//import MenuIcon from './img/MenuIcon.jpeg';
 
 var fetch = require('node-fetch');
 
-class MenuButton extends React.Component{
+/*class MenuButton extends React.Component{
 
     render(){
 
@@ -21,17 +22,84 @@ class MenuButton extends React.Component{
             </div>
         );
     }
-}
+}*/
 
-class Menu extends React.Component{
+class NavSems extends React.Component{
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            disabled1 : false,
+            disabled2 : false,
+            disabled3 : false,
+            disabled4 : false,
+            disabled5 : false,
+            disabled6 : false,
+            disabled : false
+        };
+
+
+        //this.handleMouseDown = this.handleMouseDown.bind(this);
+        //this.toggleMenu = this.toggleMenu.bind(this);
+    }
+
+    componentDidMount(){
+       // this.toggleMenu();
+       // console.log(this.state.visible);
+
+        /*if(!this.state.visible){
+            this.setState({
+                visible : true
+            });
+        }
+        else if(this.state.visible){
+            this.setState({
+                visible : false
+            });
+        }*/
+
+        if((this.props.name) === "snh")
+        {
+            this.setState({
+                disabled : true,
+                disabled3 : true,
+                disabled4 : true,
+                disabled5 : true,
+                disabled6 : true
+            });
+        }
+        else if((this.props.name) === 'others')
+        {
+            this.setState({
+                disabled1 : true,
+                disabled2  : true,
+                disabled3 : true,
+                disabled4 : true,
+                disabled5 : true,
+                disabled6 : true
+            });
+        }
+        else
+        {
+            this.setState({
+                disabled : true,
+                disabled1 : true,
+                disabled2 : true
+            });
+        }
+
+        //e.stopPropagation();
+    }
+
 
     render(){
 
-        var visibility = "hide";
+        /*var visibility = "hide";
 
         if(this.props.menuVisibility){
             visibility = "show";
-        }
+        }*/
 
         var buttonStyle = {
             height : 50,
@@ -53,44 +121,49 @@ class Menu extends React.Component{
 
         return(
 
-            <div id="slide" onMouseDown={this.props.handleMouseDown} className={visibility}>
+            <div id="slide" onMouseDown={this.handleMouseDown} style={{marginTop : 100}}>
                 <label style={labelStyle}>Contents</label>
                 <hr style={{height : 0.5, width : 500, backgroundColor : "white", color : "white"}}/>
-                <p style={{fontSize : 12, color : "white"}}>Click on the black background to close the menu</p>
+                <div>
+                    <Link to="/categorymain">
+                        <input type="image" src={LeftArrow} alt="Previous" style={{height : 50, width : 50, left : 50}}/>
+                    </Link>
+                </div>
+                {/*<p style={{fontSize : 12, color : "white"}}>Click on the black background to close the menu</p>*/}
                 <Link to={(this.props.path).concat('/phy')}>
-                <button disabled={this.props.disabled1} style={buttonStyle}>SEM-1</button>
+                <button disabled={this.state.disabled1} style={buttonStyle}>Physics Cycle</button>
                 </Link>
                 <Link to={(this.props.path).concat('/chem')}>
-                <button disabled={this.props.disabled2} style={buttonStyle}>SEM-2</button>
+                <button disabled={this.state.disabled2} style={buttonStyle}>Chemistry Cycle</button>
                 </Link>
                 <Link to={(this.props.path).concat('/sem3')}>
-                <button disabled={this.props.disabled3} style={buttonStyle}>SEM-3</button>
+                <button disabled={this.state.disabled3} style={buttonStyle}>SEM-3</button>
                 </Link>
                 <Link to={(this.props.path).concat('/sem4')}>
-                <button disabled={this.props.disabled4} style={buttonStyle}>SEM-4</button>
+                <button disabled={this.state.disabled4} style={buttonStyle}>SEM-4</button>
                 </Link>
                 <Link to={(this.props.path).concat('/sem5')}>
-                <button disabled={this.props.disabled5} style={buttonStyle}>SEM-5</button>
+                <button disabled={this.state.disabled5} style={buttonStyle}>SEM-5</button>
                 </Link>
                 <Link to={(this.props.path).concat('/sem6')}>
-                <button disabled={this.props.disabled6} style={buttonStyle}>SEM-6</button>
+                <button disabled={this.state.disabled6} style={buttonStyle}>SEM-6</button>
                 </Link>
                 <Link to='\others'>
-                <button disabled={this.props.disabled} style={buttonStyle}>Others</button>
+                <button disabled={this.state.disabled} style={buttonStyle}>Others</button>
                 </Link>
             </div>
         );
     }
 }
 
+/*className={visibility}*/
 
-class NavContainer extends React.Component{
+/*class NavContainer extends React.Component{
 
     constructor(props){
         super(props);
 
         this.state = {
-            visible : false,
             disabled1 : false,
             disabled2 : false,
             disabled3 : false,
@@ -101,12 +174,12 @@ class NavContainer extends React.Component{
         };
 
         this.handleMouseDown = this.handleMouseDown.bind(this);
-        this.toggleMenu = this.toggleMenu.bind(this);
+        //this.toggleMenu = this.toggleMenu.bind(this);
     }
 
     handleMouseDown(e){
-        //this.toggleMenu();
-        console.log(this.state.visible);
+       // this.toggleMenu();
+       // console.log(this.state.visible);
 
         if(!this.state.visible){
             this.setState({
@@ -161,8 +234,7 @@ class NavContainer extends React.Component{
     render(){
         return(
             <div>
-                <MenuButton handleMouseDown={this.handleMouseDown}/>
-                <Menu handleMouseDown={this.handleMouseDown} menuVisibility={this.state.visible} 
+                <Menu handleMouseDown={this.handleMouseDown} 
                     disabled={this.state.disabled} 
                     disabled1={this.state.disabled1} 
                     disabled2={this.state.disabled2} 
@@ -174,7 +246,11 @@ class NavContainer extends React.Component{
             </div>
         );
     }
-}
+}*/
+
+/*<MenuButton handleMouseDown={this.handleMouseDown}/>
+
+menuVisibility={this.state.visible}*/
 
 
-export default NavContainer;
+export default NavSems;
