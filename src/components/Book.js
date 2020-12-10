9bd7ec698adb738cc4323e34baed4ff2a35809b0
Rats20 <75200584+Rats20@users.noreply.github.com>
2020-12-10 07:45:51 +0530

    Update Book.js

--- src/components/Book.js
+++ src/components/Book.js
@@ -4,18 +4,25 @@ class Book extends React.Component{
     render(){
 
         var boxStyle = {
-            width : 500,
-            height : 250,
+            width : 350,
+            height : 220,
             borderRadius : 10,
             border : "5px solid #CBB5A1",
             backgroundColor : "white",
-            float : "left",
+            float : "right",
             margin : 20
         };
-        
+
+        /*var imgStyle = {
+            height : 160,
+            width : 120,
+            margin : "20px 10px",
+            float : "left"
+        };*/
+
         var letterStyle={
             fontFamily : "Arial, Times, Lucida Grande",
-            fontSize : 20,
+            fontSize : 16,
             margin : 20
         };
 
@@ -41,6 +48,7 @@ class Book extends React.Component{
 
         return(
             <div style={boxStyle}>
+                {/*<img src={this.props.source} alt="Book" style={imgStyle}/>*/}
                 <p style={letterStyle}>{this.props.tbName}</p>
                 <p style={letterStyle}>Book owner : {this.props.owner}</p>
                 <p style={letterStyle}>Contact Details : {this.props.phno}</p>
